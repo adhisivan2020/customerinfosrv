8a82a6fae4ff2191c70da1e66eb6597a41d4c9b8
segment <segment@gmail.com>
2020-06-17 14:48:43 +0530

    Cross origin code fix done

--- src/main/java/com/example/customerinfo/controller/customercontroller.java
+++ src/main/java/com/example/customerinfo/controller/customercontroller.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class customercontroller {
